5e9fcea69cf0ad6941339de5bd2004e066171586
W4veform <w4veform@w4veform.dev>
2023-02-01 14:29:39 -0700

    Set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./_site/*.html", "./_site/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
